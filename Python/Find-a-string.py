ba1b1d5b6e13b94c4768dd8d1286120b6a25be6c
Mubashir Shaheen <mubashir.shaheen17@gmail.com>
2025-03-25 14:10:12 +0500

    Find a string

---
+++ Python/Find-a-string.py
@@ -0,0 +1,60 @@
+"""
+In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
+
+NOTE: String letters are case-sensitive.
+
+Input Format
+
+The first line of input contains the original string. The next line contains the substring.
+
+Constraints
+
+
+Each character in the string is an ascii character.
+
+Output Format
+
+Output the integer number indicating the total number of occurrences of the substring in the original string.
+
+Sample Input
+
+ABCDCDC
+CDC
+Sample Output
+
+2
+Concept
+
+There are a couple of new concepts:
+In Python, the length of a string is found by the function len(s), where  is the string.
+To traverse through the length of a string, use a for loop:
+
+for i in range(0, len(s)):
+    print (s[i])
+A range function is used to loop over some length:
+
+range (0, 5)
+Here, the range loops over  to .  is excluded.
+
+Constraints
+
+-
+"""
+
+
+def count_substring(string, sub_string):
+    sub_string_len = len(sub_string)
+    string_len = len(string)
+    count = 0
+    for i in range(string_len):
+        if sub_string in string[i:sub_string_len+i]:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    string = "ABCDCDC"
+    sub_string = "CDC"
+
+    count = count_substring(string, sub_string)
+    print(count)
